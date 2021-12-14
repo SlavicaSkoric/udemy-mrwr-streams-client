70fb8482b9728c13307567aea546e1dbfc95297b
Slavica Skorić <skoric.slavica.hr@gmail.com>
2021-12-14 22:28:26 +0100

    save streak

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import { formValues } from 'redux-form';
 import streams from '../apis/streams';
 import {
   SIGN_IN,
@@ -29,3 +30,27 @@ export const createStream = (formValues) => async (dispatch) => {
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
 //post request w axios above, we put all our formValues in there
+
+export const fetchStreams = () => async (dispatch) => {
+  const response = await streams.get('/streams');
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+};

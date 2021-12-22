9df82f53e7c815e681ab7a4c0f7a482c17dc5c88
Slavica Skorić <skoric.slavica.hr@gmail.com>
2021-12-22 20:14:22 +0100

    add deleting streams functionality

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Modal from '../Modal';
 import history from '../../history';
-import { fetchStream } from '../../actions';
+import { fetchStream, deleteStream } from '../../actions';
 
 class StreamDelete extends React.Component {
   componentDidMount() {
@@ -12,10 +13,18 @@ class StreamDelete extends React.Component {
   //our component gets rendered to the screen one time; then componentDidMount runs
 
   renderActions() {
+    const { id } = this.props.match.params;
     return (
       <>
-        <button className='ui button negative'>Delete</button>
-        <button className='ui button'>Cancel</button>
+        <button
+          onClick={() => this.props.deleteStream(id)}
+          className='ui button negative'
+        >
+          Delete
+        </button>
+        <Link to='/' className='ui button'>
+          Cancel
+        </Link>
       </>
     );
   }
@@ -45,4 +54,6 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamDelete);
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);

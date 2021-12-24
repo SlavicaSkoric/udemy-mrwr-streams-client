9f0857adfca99b5c408405f2f2e6ed1b10782ad5
Slavica Skorić <skoric.slavica.hr@gmail.com>
2021-12-24 21:08:48 +0100

    create an flv player and refactor to final-form and react-final-form

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import flv from 'flv.js';
+//is about downloading video stream from rtmp server and then converting it into some file that we can play in the normal html video player
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
   }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+
+    //console.log(this.videoRef);
+
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    //console.log('I was unmounted');
+    this.player.destroy();
+  }
+  //perfect place to do some cleanup
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+  //above, options object for player configuration
 
   render() {
     if (!this.props.stream) {
@@ -16,12 +54,14 @@ class StreamShow extends React.Component {
 
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
     );
   }
 }
+//jsx elements that eventually get to represent html elements, they are not html elements (above, in the return statement)
 
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
+// import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 import streamReducer from './streamReducer';
 
 export default combineReducers({
   auth: authReducer,
-  form: formReducer,
+  // form: formReducer,
   streams: streamReducer,
 });
+
+// changes because of refactor to final-form & react-final-form
